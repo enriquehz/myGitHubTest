4cec104e2e1a5322bb860e30f5bb79b8df41991d
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-04-28 17:30:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify('All working good !!! '),
+        // TEST WITH OTHER YML FILE WORKIN'
     };
     return response;
 };
